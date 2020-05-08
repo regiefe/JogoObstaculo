9c9a1b6f312d9ed46e12f13e7ca55a824b4ac687
Reginaldo F.S <regiefe@gmail.com>
2020-05-08 10:24:37 -0300

    Alguns ajustes

---
+++ README.md
@@ -0,0 +1,6 @@
+## Jogo de obstaculo
+
+Um jogo simple de obstaculo criado apenas com canvas e animação com JavaScript
+
+![](img/jogoObstaculo.png)"Jogo de obstaculo"
+
--- js/jogo.js
+++ js/jogo.js
@@ -1,23 +1,23 @@
 let  canvas, ctx, ALTURA, LARGURA, frames = 0, maxPulos = 3, velocidade = 12, estadoAtual,
 
-estados = {
+   estados = {
     jogar: 0,
     jogando: 1,
     perdeu:2,
-},
+  },
 
-chao = {
+   chao = {
     y: 550,
     altura:50,
     cor: "#e8da78",
 
-    desenha: function(){
-        ctx.fillStyle = this.cor
-        ctx.fillRect( 0, this.y, LARGURA, this.altura )
+    desenha(){
+      ctx.fillStyle = this.cor
+      ctx.fillRect( 0, this.y, LARGURA, this.altura )
     },
-},
+  },
 
-bloco = {
+   bloco = {
     x: 50,
     y: 0,
     altura:50,
@@ -29,154 +29,179 @@ bloco = {
     qntPulos: 0,
     score: 0,
 
-    atualiza:function(){
-        this.velocidade += this.gravidade
-        this.y += this.velocidade
+    atualiza(){
+      this.velocidade += this.gravidade
+      this.y += this.velocidade
 
-        if( this.y > chao.y - this.altura && estadoAtual != estados.perdeu  ){
-            this.y = chao.y - this.altura 
-            this.qntPulos =  0    
-            this.velocidade = 0
-        }
-    },
-
-    pula: function (){
-        if(this.qntPulos < maxPulos){
-            this.velocidade = -this.forcaDoPulo
-            this.qntPulos++
-        }
-    },
-
-    reset: function (){
+      if( this.y > chao.y - this.altura && estadoAtual != estados.perdeu  ){
+        this.y = chao.y - this.altura 
+        this.qntPulos =  0    
         this.velocidade = 0
-        this.y  = 0
-        this.scorre = 0
+      }
     },
 
-    desenha: function(){
-        ctx.fillStyle = this.cor
-        ctx.fillRect( this.x, this.y, this.altura, this.largura )
-    },
-}
-
-obstaculos = {
-    _obs: [],
-    cores: ["#ffbc1c", "#ff1c1c", "#ff85e1", "#52a7ff", "#78ff4d"],
-    tempoInsere: 0,
-
-    insere: function() {
-        this._obs.push({
-            x: LARGURA,
-            largura: 50,
-            altura: 30 + Math.floor(120 * Math.random()),
-            cor: this.cores[Math.floor(5 * Math.random())],
-        })
-        this.tempoInsere = 30 + Math.floor(22 * Math.random())
-    },
-    atualiza: function() {
-        if(this.tempoInsere ==  0)
-            this.insere()
-        else
-            this.tempoInsere--
-
-        for(var i = 0, tam = this._obs.length; i < tam; i++){
-            var obs = this._obs[i]
-            obs.x -= velocidade
-
-            if( bloco.x < obs.x + obs.largura &&
-                bloco.x + bloco.largura >= obs.x &&
-                bloco.y + bloco.altura >= chao.y - obs.altura)
-                estadoAtual = estados.perdeu
-
-            else if(obs.x == 0)
-                bloco.score++
-
-            else if(obs.x <= -obs.largura){
-                this._obs.splice(i, 1)
-                tam--
-                i--
-            }
-        }
-    },
-    limpa: function(){
-        this._obs = []
+    pula(){
+      if(this.qntPulos < maxPulos){
+        this.velocidade = -this.forcaDoPulo
+        this.qntPulos++
+      }
     },
 
-    desenha: function() {
-        for(var i = 0, tam = this._obs.length; i < tam; i++) {
-            var obs = this._obs[i]
-            ctx.fillStyle = obs.cor
-            ctx.fillRect(obs.x, chao.y - obs.altura, obs.largura, obs.altura)
-        }
+    reset(){
+      this.velocidade = 0
+      this.y  = 0
+      this.scorre = 0
     },
+
+    desenha(){
+      ctx.fillStyle = this.cor
+      ctx.fillRect( this.x, this.y, this.altura, this.largura )
+    },
+  },
+
+ obstaculos = {
+  _obs: [],
+  cores: ["#ffbc1c", "#ff1c1c", "#ff85e1", "#52a7ff", "#78ff4d"],
+  tempoInsere: 0,
+
+  insere() {
+    this._obs.push({
+      x: LARGURA,
+      largura: 50,
+      altura: 30 + Math.floor(120 * Math.random()),
+      cor: this.cores[Math.floor(5 * Math.random())],
+    })
+    this.tempoInsere = 30 + Math.floor(22 * Math.random())
+  },
+
+  atualiza() {
+    if(this.tempoInsere ==  0)
+      this.insere()
+    else
+      this.tempoInsere--
+
+    for(let i = 0, tam = this._obs.length; i < tam; i++){
+      let obs = this._obs[i]
+      obs.x -= velocidade
+
+      if( bloco.x < obs.x + obs.largura &&
+        bloco.x + bloco.largura >= obs.x &&
+        bloco.y + bloco.altura >= chao.y - obs.altura)
+        estadoAtual = estados.perdeu
+
+      else if(obs.x == 0)
+        bloco.score++
+
+      else if(obs.x <= -obs.largura){
+        this._obs.splice(i, 1)
+        tam--
+        i--
+      }
+    }
+  },
+
+  limpa(){
+    this._obs = []
+  },
+
+  desenha() {
+    for(let i = 0, tam = this._obs.length; i < tam; i++) {
+      let obs = this._obs[i]
+      ctx.fillStyle = obs.cor
+      ctx.fillRect(obs.x, chao.y - obs.altura, obs.largura, obs.altura)
+    }
+  },
 }
 
 function clique(){
-    if(estadoAtual == estados.jogando)
-        bloco.pula()
+  if(estadoAtual == estados.jogando)
+    bloco.pula()
 
-    else if(estadoAtual == estados.jogar)
-        estadoAtual = estados.jogando
+  else if(estadoAtual == estados.jogar)
+    estadoAtual = estados.jogando
 
-    else if(estadoAtual == estados.perdeu && bloco.y >= 2 * ALTURA){
-        estadoAtual = estados.jogar
-        obstaculos.limpa()
-        bloco.reset()
-    }
+  else if(estadoAtual == estados.perdeu && bloco.y >= 2 * ALTURA){
+    estadoAtual = estados.jogar
+    obstaculos.limpa()
+    bloco.reset()
+  }
 }
+
 function roda(){
 
-    atualiza()
-    desenha()
+  atualiza()
+  desenha()
 
-    window.requestAnimationFrame(roda)
+  window.requestAnimationFrame(roda)
 }
+
 function atualiza(){
-    frames++
-    bloco.atualiza()
-    if ( estadoAtual == estados.jogando)
-        obstaculos.atualiza()
+  frames++
+  bloco.atualiza()
+  if ( estadoAtual == estados.jogando)
+    obstaculos.atualiza()
 }
 
 function desenha() {
-    ctx.fillStyle = "#80daff"
-    ctx.fillRect(0, 0, LARGURA, ALTURA)
+  ctx.fillStyle = "#80daff"
+  ctx.fillRect(0, 0, LARGURA, ALTURA)
 
-    ctx.fillStyle = "#fff"
-    ctx.font = "50px Arial"
-    ctx.fillText( bloco.score, 30, 68 )
+  ctx.fillStyle = "#fff"
+  ctx.font = "50px Arial"
+  ctx.fillText( bloco.score, 30, 68 )
 
 
-    switch (estadoAtual){
+  switch (estadoAtual){
 
-        case estados.jogar:
-            ctx.fillStyle = "green"
-            ctx.fillRect(LARGURA / 2 - 50, ALTURA / 2 - 50, 100, 100)
-            break
-        case estados.perdeu:
-            ctx.fillStyle = "red"
-            ctx.fillRect(LARGURA / 2 - 50, ALTURA / 2 - 50, 100, 100)
+    case estados.jogar:
+      ctx.fillStyle = "green"
+      ctx.fillRect(LARGURA / 2 - 50, ALTURA / 2 - 50, 100, 100)
+      break
+    case estados.perdeu:
+      ctx.fillStyle = "red"
+      ctx.fillRect(LARGURA / 2 - 50, ALTURA / 2 - 50, 100, 100)
 
-            ctx.save()
-            ctx.translate(LARGURA / 2, ALTURA / 2)
-            ctx.fillStyle = "#fff"
+      ctx.save()
+      ctx.translate(LARGURA / 2, ALTURA / 2)
+      ctx.fillStyle = "#fff"
 
-            if(bloco.score < 10)
-                ctx.fillText(bloco.score, -13, 19)
-            else if(bloco.score >= 10 && bloco.score < 100) 
-                ctx.fillText(bloco.score, -26, 19)
-            else
-                ctx.fillText(bloco.score, -39, 19)
+      if(bloco.score < 10)
+        ctx.fillText(bloco.score, -13, 19)
+      else if(bloco.score >= 10 && bloco.score < 100) 
+        ctx.fillText(bloco.score, -26, 19)
+      else
+        ctx.fillText(bloco.score, -39, 19)
 
-            ctx.restore()
-            break
-        case estados.jogando:
-            obstaculos.desenha()
-            break
-    }
+      ctx.restore()
+      break
+    case estados.jogando:
+      obstaculos.desenha()
+      break
+  }
 
-    chao.desenha()
-    bloco.desenha()
+  chao.desenha()
+  bloco.desenha()
 }
 
+function main() {
+    ALTURA  = window.innerHeight
+    LARGURA = window.innerWidth
+
+    if(LARGURA >= 500){
+        LARGURA = 600;
+        ALTURA = 600;
+    }
+
+    canvas = document.createElement("canvas")
+    canvas.width = LARGURA
+    canvas.height = ALTURA
+    canvas.style.border = "1px solid #000"
+
+    ctx = canvas.getContext("2d")
+    document.body.appendChild(canvas)
+    document.addEventListener("mousedown", clique)
+
+    estadoAtual = estados.jogar
+    roda()
+}
 main()
